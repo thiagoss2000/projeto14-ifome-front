65f2592d9ee43667ab42c51ec899b76cd3d3c3e5
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-05-15 12:48:50 -0300

    base for axios get and post in checkout

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,8 +1,22 @@
+import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+import axios from "axios"
 import styled from "styled-components"
 
 export default function Checkout() {
+  const [success, setSuccess] = useState(false)
+  const [checkout, setCheckout] = useState({})
+
   const navigate = useNavigate()
+  // const config = { Authorization: `Bearer ${user.token}` } !FIX ME PLEASE
+  const config = { Authorization: `Bearer ...` }
+
+  useEffect(() => {
+    const URI = "http://localhost:5000/checkout"
+    const promisse = axios.get(URI, { headers: config })
+    promisse.then((response) => setCheckout(response))
+    promisse.catch((e) => console.error(e))
+  }, [])
 
   function returnMainPage() {
     const confirm = window.confirm("Do you want to return to the main page?")
@@ -12,10 +26,15 @@ export default function Checkout() {
   }
 
   function confirmCheckout() {
-    console.log("foi")
+    const URI = `http://localhost:5000/checkout/${checkout._id}`
+    const promisse = axios.post(URI, { headers: config })
+    promisse.then((response) => setSuccess(true))
+    promisse.catch((e) => console.error(e))
   }
 
-  const url = "https://img.itdg.com.br/tdg/images/blog/uploads/2017/07/shutterstock_413580649.jpg?w=1200"
+  let total = 0
+  checkout.shopping?.forEach((p) => (total += parseFloat(p.value)))
+
   return (
     <Main>
       <Header>
@@ -24,14 +43,33 @@ export default function Checkout() {
       </Header>
       <h1>Checkout</h1>
       <section>
-        <Product>
-          <img src={url} alt="comida"></img>
-          <p>Macarrão com molho tomate</p>
-          <var>R$ 20.99</var>
-        </Product>
+        {checkout.shopping?.map((p) => {
+          return (
+            <Product>
+              <img src={p.img} alt="comida"></img>
+              <p>{p.title}</p>
+              <var>R$ {p.value}</var>
+            </Product>
+          )
+        })}
       </section>
-      <h4>Total: R$20.99</h4>
-      <button onClick={confirmCheckout}>Confirm</button>
+      <h4>Total: R${total.toFixed(2)}</h4>
+      <button
+        onClick={checkout.shopping ? confirmCheckout : null}
+        style={checkout.shopping ? {} : { opacity: 0.6, cursor: "auto" }}
+      >
+        Confirm
+      </button>
+      {success ? (
+        <Success>
+          <article>
+            Success!
+            <button onClick={() => navigate("/main")}>Return to Main Page</button>
+          </article>
+        </Success>
+      ) : (
+        <></>
+      )}
     </Main>
   )
 }
@@ -129,6 +167,7 @@ const Product = styled.article`
     max-width: 80px;
 
     margin-right: 10px;
+    border-radius: 5px;
   }
 
   p {
@@ -142,5 +181,36 @@ const Product = styled.article`
   var {
     position: absolute;
     right: 10px;
+  }
+`
+
+const Success = styled.section`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  z-index: 20;
+  background-color: rgba(0, 0, 0, 0.5);
+
+  article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+
+    width: 85%;
+    max-width: 400px;
+    height: 40%;
+
+    border-radius: 20px;
+    background-color: #ffffff;
+
+    font-size: 30px;
+    font-weight: 700;
+    color: var(--theme);
   }
 `
